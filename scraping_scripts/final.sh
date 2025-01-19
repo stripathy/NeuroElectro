fc2364535bddb6764fa1764dc732dcb0b9e3b510
hudsonhu22 <hugohudson@me.com>
2025-01-19 18:39:06 -0500

    all patterns done

---
+++ scraping_scripts/final.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Directory containing .html files
+folder_path="/Users/hugohudson/Downloads/NeuroElectro-main-8/datatable"
+
+# Iterate over all .html files in the directory and subdirectories
+find "$folder_path" -type f -name "*.html" | while read -r file; do
+  if [[ -f "$file" ]]; then
+    # Modify the content of each file
+    # sed -i '' -E \
+    #   -e 's|(data_table/[0-9]+/)|../../../\1|g' \
+    #   -e 's|(neuron/[0-9]+/)|../../../\1|g' \
+    #   -e 's|(article/[0-9]+/)|../../../\1|g' \
+    #   -e 's|(ephys_prop/[0-9]+/)|../../../\1|g' \
+    #   "$file"
+    sed -i '' -E \
+      -e 's|../../../(data_table/[0-9]+/)|../../\1|g' \
+      -e 's|../../../(neuron/[0-9]+/)|../../\1|g' \
+      -e 's|../../../(article/[0-9]+/)|../../\1|g' \
+      -e 's|../../../(ephys_prop/[0-9]+/)|../../\1|g' \
+      "$file"
+    echo "Updated $file"
+  fi
+done
--- scraping_scripts/redopath.sh
+++ scraping_scripts/redopath.sh
@@ -8,14 +8,20 @@ find "$folder_path" -type f -name "*.html" | while read file; do
   if [[ -f "$file" ]]; then
     sed -i '' -E \
     -e 's|https://web.archive.org/web/[0-9]+/||g' \
-    -e 's|/web/20211201101926/https://neuroelectro.org/||g' \
+    -e 's|/web/[0-9]+/https://neuroelectro.org/||g' \
+    -e 's|/web/[0-9]+/https://www.neuroelectro.org/||g' \
     -e 's|https://neuroelectro.org/article/neuron/([0-9]+)|../../../article/neuron/\1.html|g' \
     -e 's|https://neuroelectro.org/data_table/neuron/([0-9]+)|../../../datatable/neuron/\1.html|g' \
     -e 's|https://neuroelectro.org/article/ephys_prop/([0-9]+)|../../../article/ephys_prop/\1.html|g' \
     -e 's|https://neuroelectro.org/data_table/ephys_prop/([0-9]+)|../../../datatable/ephys_prop/\1.html|g' \
     -e 's|https://neuroelectro.org/neuron/([0-9]+)|../../../neuron/\1.html|g' \
     -e 's|https://neuroelectro.org/ephys_prop/([0-9]+)|../../../ephys_prop/\1.html|g' \
-
+    -e 's|https://www.neuroelectro.org/article/neuron/([0-9]+)|../../../article/neuron/\1.html|g' \
+    -e 's|https://www.neuroelectro.org/data_table/neuron/([0-9]+)|../../../datatable/neuron/\1.html|g' \
+    -e 's|https://www.neuroelectro.org/article/ephys_prop/([0-9]+)|../../../article/ephys_prop/\1.html|g' \
+    -e 's|https://www.neuroelectro.org/data_table/ephys_prop/([0-9]+)|../../../datatable/ephys_prop/\1.html|g' \
+    -e 's|https://www.neuroelectro.org/neuron/([0-9]+)|../../../neuron/\1.html|g' \
+    -e 's|https://www.neuroelectro.org/ephys_prop/([0-9]+)|../../../ephys_prop/\1.html|g' \
     "$file"
     echo "Updated $file"
   fi
