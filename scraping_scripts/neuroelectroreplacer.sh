4e39e5d999b1b86ad648bfb0e69359d96aecb7fe
hudsonhu22 <hugohudson@me.com>
2025-01-21 14:02:25 -0500

    flipped

--- scraping_scripts/neuroelectroreplacer.sh
+++ scraping_scripts/neuroelectroreplacer.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
 
 # Define the directory to process
-DIR="/Users/hugohudson/Downloads/NeuroElectro-main-4/data/ephys_prop"
-
+DIR="/Users/hugohudson/Downloads/NeuroElectro-main-8/datatable/neuron2"
+# make sure only 2 ../ for neuron data
 # Define search and replacement strings
 SEARCH_REPLACE=(
-    "https://neuroelectro.org/article/index ../../article/index.html"
-    "https://neuroelectro.org/api/docs ../../api/docs.html"
-    "https://neuroelectro.org/contribute ../../contribute.html"
-    "https://neuroelectro.org/ephys_prop/index ../../ephys_prop/index.html"
-    "https://neuroelectro.org/neuron/index ../../neuron/index.html"
-    "https://neuroelectro.org/publications ../../publications.html"
-    "https://neuroelectro.org/faqs ../../faqs.html"
-    "https://neuroelectro.org/neuron/clustering ../../neuron/clustering.html"
+    "https://neuroelectro.org/article/index ../../../article/index.html"
+    "https://neuroelectro.org/api/docs ../../../api/docs.html"
+    "https://neuroelectro.org/contribute ../../../contribute.html"
+    "https://neuroelectro.org/ephys_prop/index ../../../ephys_prop/index.html"
+    "https://neuroelectro.org/neuron/index ../../../neuron/index.html"
+    "https://neuroelectro.org/publications ../../../publications.html"
+    "https://neuroelectro.org/faqs ../../../faqs.html"
+    "https://neuroelectro.org/neuron/clustering ../../../neuron/clustering.html"
+    "https://neuroelectro.org/neuron_search ../../../neuron_search.html"
+    "https://www.neuroelectro.org/article/index ../../../article/index.html"
+    "https://www.neuroelectro.org/api/docs ../../../api/docs.html"
+    "https://www.neuroelectro.org/contribute ../../../contribute.html"
+    "https://www.neuroelectro.org/ephys_prop/index ../../../ephys_prop/index.html"
+    "https://www.neuroelectro.org/neuron/index ../../../neuron/index.html"
+    "https://www.neuroelectro.org/publications ../../../publications.html"
+    "https://www.neuroelectro.org/faqs ../../../faqs.html"
+    "https://www.neuroelectro.org/neuron/clustering ../../../neuron/clustering.html"
+    "https://www.neuroelectro.org/neuron_search ../../../neuron_search.html"
 )
 REGEX_REPLACE=(
     "https://web.archive.org/web/[0-9]{14}/"
 )
 
 # Define the new regex for neuron/ephys_prop data links
-SEARCH10="https://neuroelectro.org/\(article\|data_table\)/\([0-9]\{5\}\)/"
-REPLACE10="../datatable/ephys_prop/\2/data/index.html"
+# SEARCH10="https://www.neuroelectro.org/data_table/([0-9]+)/"
+# REPLACE10="../../../../../datatable/ephys_prop/\1.html"
+
+# SEARCH19="https://www.neuroelectro.org/article/([0-9]+)/"
+# REPLACE19="../../../../../article/ephys_prop/\1.html"
+
+SEARCH11="web.archive.org/web/([0-9]{14})"
+REPLACE11="https://web.archive.org/web/\1"
+
+SEARCH121="/web/([0-9]{14})/"
+REPLACE121="https://web.archive.org/web/\1"
+
+SEARCH13="https://www.neuroelectro.org/neuron/([0-9]+)/"
+REPLACE13="../../../neuron/\1.html"
+
+SEARCH14="https://www.neuroelectro.org/ephys_prop/([0-9]+)/"
+REPLACE14="../../../ephys_prop/\1.html"
+
+SEARCH10="https://www.neuroelectro.org/data_table/([0-9]+)/"
+REPLACE10="../../../datatable/neuron/\1.html"
+
+SEARCH19="https://www.neuroelectro.org/article/([0-9]+)/"
+REPLACE19="../../../article/ephys_prop/\1.html"
+
+SEARCH23="https://www.neuroelectro.org/neuron/([0-9]+)/"
+REPLACE23="../../../neuron/\1.html"
+
+SEARCH24="https://www.neuroelectro.org/ephys_prop/([0-9]+)/"
+REPLACE24="../../../ephys_prop/\1.html"
 
 # Check if the directory exists
 if [ ! -d "$DIR" ]; then
@@ -45,7 +82,12 @@ for FILE in "$DIR"/*; do
 
         # Perform the specific replacement for neuron/ephys_prop URLs, retaining the number
         sed -E -i '' "s|$SEARCH10|$REPLACE10|g" "$FILE"
-
+        # sed -E -i '' "s|$SEARCH11|$REPLACE11|g" "$FILE"
+        sed -E -i '' "s|$SEARCH121||g" "$FILE"
+        sed -E -i '' "s|$SEARCH19|$REPLACE19|g" "$FILE"
+        sed -E -i '' "s|$SEARCH13|$REPLACE13|g" "$FILE"
+        sed -E -i '' "s|$SEARCH14|$REPLACE14|g" "$FILE"
+        
         echo "Modified: $FILE"
     fi
 done
